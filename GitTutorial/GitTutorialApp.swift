3d0fc9240cf942f50615533c1865eff0011a6503
Charlyboy <estradamirandacarlos@gmail.com>
2024-02-28 14:38:20 -0600

    Initial Commit

---
+++ GitTutorial/GitTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTutorialApp.swift
+//  GitTutorial
+//
+//  Created by Carlos Estrada on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GitTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
